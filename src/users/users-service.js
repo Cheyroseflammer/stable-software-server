f49c0bf9c6744a17f7bb54b6a76ca935511b2035
Chey Rose Flammer <cheyroseflammer@gmail.com>
2020-11-19 12:26:08 -0800

    can post to users, riders, and horses

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -1,20 +1,51 @@
-const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&])[\S]+/;
+const express = require('express');
+const xss = require('xss');
+const bcrypt = require('bcryptjs');
 
+// to store db transactions:
 const UsersService = {
+  serializeUser(user) {
+    return {
+      id: user.id,
+      email: xss(user.email),
+    };
+  },
+  getAllUsers(knex) {
+    return knex.select('*').from('stable_users');
+  },
+  hasUserWithUserName(db, email) {
+    return db('stable_users')
+      .where({ email })
+      .first()
+      .then((user) => !!user);
+  },
+  insertUser(db, newUser) {
+    return db
+      .insert(newUser)
+      .into('stable_users')
+      .returning('*')
+      .then(([user]) => user);
+  },
   validatePassword(password) {
     if (password.length < 8) {
-      return `Password must be longer than 8 characters`;
+      return 'Password must be longer than 8 characters';
     }
-    if (password.length > 72) {
-      return `Password must be less than 72 characters long`;
+    if (password.length > 20) {
+      return 'Password must be less than 20 characters';
     }
     if (password.startsWith(' ') || password.endsWith(' ')) {
-      return `Password must not start or end with empty space`;
+      return 'Password must not start or end with empty spaces';
     }
-    if (!REGEX_UPPER_LOWER_NUMBER_SPECIAL.test(password)) {
-      return 'Password must contain 1 upper case, lower case, number and special character';
-    }
-    return null;
+  },
+  hashPassword(password) {
+    return bcrypt.hash(password, 12);
+  },
+  deleteUser(knex, id) {
+    return knex('stbale_users').where({ id }).delete();
+  },
+  getById(knex, id) {
+    return knex.from('stable_users').select('*').where('id', id).first();
   },
 };
+
 module.exports = UsersService;
