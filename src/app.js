33ca9519c4c01a63a9cd3307cf1574cb5658b444
Cheyroseflammer <cheyroseflammer@gmail.com>
2020-11-28 11:17:06 -0800

    deploy

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
-const { NODE_ENV } = require('./config');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
 
 // Routers
-const ridersRouter = require('./riders/riders-router');
-const horsesRouter = require('./horses/horses-router');
-const authRouter = require('./auth/auth-router');
-const usersRouter = require('./users/users-router');
+const ridersRouter = require("./riders/riders-router");
+const horsesRouter = require("./horses/horses-router");
+const authRouter = require("./auth/auth-router");
+const usersRouter = require("./users/users-router");
 
 const app = express();
 
-const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
+const morganOption = process.env.NODE_ENV === "production" ? "tiny" : "common";
 app.use(helmet());
 
 app.use(morgan(morganOption));
@@ -25,20 +25,20 @@ app.use(
 
 // Router Uses
 
-app.use('/api/riders', ridersRouter);
-app.use('/api/users', usersRouter);
-app.use('/api/horses', horsesRouter);
-app.use('/api/auth', authRouter);
+app.use("/api/riders", ridersRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/horses", horsesRouter);
+app.use("/api/auth", authRouter);
 // app.use('/api/*', app);
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
 });
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
-  if (process.env.NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } };
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
   } else {
     console.error(error);
     response = { message: error.message, error };
--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 9000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-  API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
+  NODE_ENV: process.env.NODE_ENV || "development",
+  JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
+  API_TOKEN: process.env.API_TOKEN,
 };
